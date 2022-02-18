0100589d16df13365623334a2079820ce2cd8bfb
QwQ2000 <396707050@qq.com>
2022-02-18 15:03:19 +0800

    poseformer model

---
+++ tests/test_backbones/test_poseformer.py
@@ -0,0 +1,13 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+from mmpose.models import builder
+
+
+def test_poseformer():
+    """Test PoseFormer."""
+    cfg = dict(type='PoseFormer')
+    model = builder.build_backbone(*cfg)
+    input = torch.randn(16, 9, 17, 2)
+    output = model(input)
+    assert output.shape == (16, 17, 3)
